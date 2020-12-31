78cb38fc1a933f2f92a5afea9852d9961ca9990e
Rushabh Shah <rushabh.shah5@wipro.com>
2020-12-31 15:18:49 +0530

    chjat app done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,111 @@
 const socket = io();
 
-socket.on('message', (msg) => {
-    console.log(msg);
+// Elements
+const $msgBox = document.querySelector('#msgBox');
+const $sendButton = document.querySelector('#sendButton');
+const $sendLocation = document.querySelector('#sendLocation');
+const $messages = document.querySelector('#messages');
+const $sidebar = document.querySelector('#sidebar');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector('#location-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+socket.on('message', (message) => {
+    createMessage(messageTemplate, message);
 });
 
-document.querySelector('#sendButton').addEventListener('click', () => {
-    const msg = document.querySelector('#msgBox').value;
-    socket.emit('sendMessage', msg, (error) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message delivered!');
+socket.on('locationMessage', (message) => {
+    createMessage(locationTemplate, message);
+});
+
+socket.on('roomData', ({ room, users }) => {
+    console.log('room', room);
+    console.log('users', users);
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
     });
+    $sidebar.innerHTML = html;
+})
+
+function createMessage(template, message) {
+    console.log(message);
+    const html = Mustache.render(template, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('hh:mm A'),
+        username: message.username
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
+}
+
+const autoScroll = () => {
+    const $newMessage = $messages.lastElementChild;
+
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMarginBottom = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMarginBottom;
+
+    const visibleHeight = $messages.offsetHeight;
+
+    const containerHeight = $messages.scrollHeight;
+
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+
+
+
+}
+
+$sendButton.addEventListener('click', () => {
+    const msg = $msgBox.value;
+
+    if (msg.trim()) {
+        $sendButton.setAttribute('disabled', 'disabled');
+
+        socket.emit('sendMessage', msg, (error) => {
+            $sendButton.removeAttribute('disabled');
+            $msgBox.value = '';
+            $msgBox.focus();
+
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message delivered!');
+        });
+    }
 });
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$sendLocation.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation not suppported')
     }
 
+    $sendLocation.setAttribute('disabled', 'disabled');
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocation.removeAttribute('disabled');
             console.log('Location shared!')
         });
     });
 
+});
+
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error);
+        location.href = '/';
+    }
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app);
@@ -16,8 +18,32 @@ app.use(express.static(publicDirecotryPath));
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
 
-    socket.emit('message', 'Welcome! Connection successful.');
-    socket.broadcast.emit('message', 'New user joined');
+    // socket.emit() - to particular connection
+    // io.emit() - to every connection
+    // socket.broadcast.emit - to all but socket
+    // io.to().emit() - to particular room
+    // socket.broadcast.to().emit() - to all in particular room but socket
+
+    socket.on('join', (options, callback) => {
+
+        const { error, user } = addUser({ id: socket.id, ...options });
+
+        if (error) {
+            return callback(error);
+        }
+
+        socket.join(user.room);
+
+        socket.emit('message', generateMessage('Admin', `Welcome! ${user.username}`));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} joined!`));
+
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
+
+        callback();
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter();
@@ -26,18 +52,28 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed.');
         }
 
-        io.emit('message', message);
-        callback();
-    });
+        const user = getUser(socket.id);
 
-    socket.on('disconnect', () => {
-        io.emit('message', 'User left')
+        io.to(user.room).emit('message', generateMessage(user.username, message));
+        callback();
     });
 
     socket.on('sendLocation', (location, callback) => {
-        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, location));
         callback();
     })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} left!`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
+        }
+    });
 })
 
 server.listen(port, () => {
